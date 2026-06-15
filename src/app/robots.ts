f485a8f2572051c2da848a7a28f981513ee26fb4
unknown <muthurasu6319@gmail.com>
2026-06-15 23:28:56 +0530

    second commit

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/', '/admin/', '/private/'],
     },
-    sitemap: 'https://genzneuralx.com/sitemap.xml',
+    sitemap: 'https://gen-z-neural-x.vercel.app/sitemap.xml',
   }
 }
