d2f3020a794963fd77694ee5eb9e111d6af50f86
unknown <muthurasu6319@gmail.com>
2026-07-02 16:58:39 +0530

    google ads updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -124,6 +124,9 @@ export default function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1655533057008648" crossOrigin="anonymous"></script>
+      </head>
       <body suppressHydrationWarning>
         <script
           type="application/ld+json"
