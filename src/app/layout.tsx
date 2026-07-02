5c10b7b0158941a6699760da802f82fbddacba0a
unknown <muthurasu6319@gmail.com>
2026-07-02 22:58:50 +0530

    fix typo in adsense script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -128,7 +128,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4559745172452836 " crossOrigin="anonymous"></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4559745172452836" crossOrigin="anonymous"></script>
       </head>
       <body suppressHydrationWarning>
         <script
